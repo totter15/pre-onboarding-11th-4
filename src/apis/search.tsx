d53d143599bc3054878df4ea2bf8a39ab7eca4a8
천진아 <totter15@naver.com>
2023-07-17 23:06:07 +0900

    feat : api 호출시 console.info 출력

--- src/apis/search.tsx
+++ src/apis/search.tsx
@@ -6,6 +6,7 @@ export interface SearchItem {
 }
 
 export async function getSearch(query: string): Promise<SearchItem[]> {
+	console.info('calling api');
 	const { data } = await client.get(`?q=${query}`);
 	return data;
 }
